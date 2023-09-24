468fb08fbe7470c7c8f6e06cba3d59220c8dcb62
Mgphone <51868037+Mgphone@users.noreply.github.com>
2023-09-24 08:36:05 +0100

    add blackjack project and javascript file

---
+++ javascript.js
@@ -0,0 +1,7 @@
+const languages = document.querySelectorAll(".language");
+languages.forEach((languages) => {
+  const spans = languages.querySelectorAll("span");
+  for (let i = 0; i < spans.length; i++) {
+    spans[i].classList.add("text-bg-success");
+  }
+});
