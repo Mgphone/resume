42ff1788c031b00dff4d704dc794a713fb10f9c7
Mgphone <51868037+Mgphone@users.noreply.github.com>
2023-09-24 18:34:27 +0100

    update link BlackJack,some css

--- javascript.js
+++ javascript.js
@@ -1,7 +1,10 @@
-const languages = document.querySelectorAll(".language");
-languages.forEach((languages) => {
-  const spans = languages.querySelectorAll("span");
-  for (let i = 0; i < spans.length; i++) {
-    spans[i].classList.add("text-bg-success");
-  }
+document.addEventListener("DOMContentLoaded", function () {
+  // Your JavaScript code here
+  const languages = document.querySelectorAll(".language");
+  languages.forEach((languages) => {
+    const spans = languages.querySelectorAll("span");
+    for (let i = 0; i < spans.length; i++) {
+      spans[i].classList.add("text-bg-success");
+    }
+  });
 });
