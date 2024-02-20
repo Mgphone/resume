5ba8a2fa79eabc5d1be8b8c5f13f1e81f58e8f61
Mgphone <51868037+Mgphone@users.noreply.github.com>
2024-02-20 21:42:02 +0000

    add json file for projects

--- resume-app/pages/index.js
+++ resume-app/pages/index.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import style from "../styles/Index.module.css";
+import data from "../data/data.json";
 function index() {
   // const welcometitle = " Passionate Self-Taught Web Developer ";
   return (
@@ -39,7 +40,9 @@ function index() {
           </p>
           <p>
             My passion lies in creating dynamic web experiences. I'm skilled in
-            JavaScript, HTML, CSS, React, MongoDB, and Git.
+            JavaScript, HTML, CSS, React, MongoDB, and Git, with additional
+            expertise in Node.js, Express.js, JWT (JSON Web Tokens), and
+            Next.js.
           </p>
           <p>
             Let's bring your ideas to life and build exceptional web solutions
@@ -50,184 +53,33 @@ function index() {
       <section id="projects" className={style.projects}>
         <h1>Some of My Projects</h1>
         <div className={style["projects-content"]}>
-          <div className={style["project-article"]}>
-            <h3 className={style["project-tile"]}>Removal Company</h3>
-            <img
-              src="https://res.cloudinary.com/dsigqr3ht/image/upload/v1707268549/Porfolio/TurboRemoval.jpg"
-              alt="Restaurant"
-            />
-            <h4>Languages</h4>
-            <p className={style.language}>
-              <span>HTML </span>
-              <span>CSS</span>
-              <span>React</span>
-              <span>JavaScript</span>
-              <span>API</span>
-              <span>Git</span>
-              <span>MongoDB</span>
-            </p>
-            <p>
-              <span>
-                <a href="https://turboremovals.netlify.app/" target="_blank">
-                  Link For Web
-                </a>
-              </span>
-              <span>
-                <a
-                  href="https://github.com/Mgphone/TurboRemoval"
-                  target="_blank"
-                >
-                  Source Code
-                </a>
-              </span>
-            </p>
-          </div>
-          {/* <div class="project-article">
-            <h3 class="project-tile">Ecommerce</h3>
-            <img
-              src="https://res.cloudinary.com/dsigqr3ht/image/upload/v1697717755/Porfolio/Screenshot_2023-10-19.png"
-              alt="Restaurant"
-            />
-            <h4>Languages</h4>
-            <p class="language">
-              <span>HTML </span>
-              <span>CSS</span>
-              <span>React</span>
-              <span>JavaScript </span>
-              <span>API</span>
-              <span>Git</span>
-            </p>
-            <p>
-              <span>
-                <a href="https://sampleasiavilla.netlify.app/" target="_blank">
-                  Link For Web
-                </a>
-              </span>
-              <span>
-                <a href="https://github.com/Mgphone/AsiaReact" target="_blank">
-                  Source Code
-                </a>
-              </span>
-            </p>
-          </div>
-          <div class="project-article">
-            <h3 class="project-tile">Ecommerce</h3>
-            <img
-              src="https://res.cloudinary.com/dsigqr3ht/image/upload/v1693074975/Porfolio/crubasia_meiv66.png"
-              alt="Ecommerce"
-            />
-            <h4>Languages</h4>
-            <p class="language">
-              <span>HTML </span>
-              <span>CSS</span>
-              <span>EJS</span>
-              <span>JavaScript </span>
-              <span>NodeJS</span>
-              <span>MongoDB</span>
-            </p>
-            <p>
-              <span>
-                <a href="https://mgphoneshopping.onrender.com/" target="_blank">
-                  Link For Web
-                </a>
-              </span>
-              <span>
-                <a href="https://github.com/Mgphone/-asiacart-" target="_blank">
-                  Source Code
-                </a>
-              </span>
-            </p>
-          </div>
-          <div class="project-article">
-            <h3 class="project-tile">Cocktails App</h3>
-            <img
-              src="https://res.cloudinary.com/dsigqr3ht/image/upload/v1693074975/Porfolio/cocktail_my8gfw.png"
-              alt="Project Cocktail"
-            />
-            <h4>Languages</h4>
-            <p class="language">
-              <span>React </span>
-              <span>API</span>
-            </p>
-            <p>
-              <span>
-                <a href="https://mgphonecocktails.netlify.app/" target="_blank">
-                  Link For Web
-                </a>
-              </span>
-              <span>
-                <a
-                  href="https://github.com/Mgphone/mgphonecocktail"
-                  target="_blank"
-                >
-                  Source Code
-                </a>
-              </span>
-            </p>
-          </div>
-
-          <div class="project-article">
-            <h3 class="project-tile">Black Jack</h3>
-            <img
-              src="https://res.cloudinary.com/dsigqr3ht/image/upload/v1695536755/Porfolio/Screenshot_2023-09-24_071827_si7wyy.png"
-              alt="Black Jack"
-            />
-            <h4>Languages</h4>
-            <p class="language">
-              <span>HTML </span>
-              <span>CSS</span>
-              <span>JavaScript</span>
-              <span>DOM</span>
-            </p>
-            <p>
-              <span>
-                <a href="https://mgphone.w3spaces.com/" target="_blank">
-                  Link For Web
-                </a>
-              </span>
-              <span>
-                <a
-                  href="https://github.com/Mgphone/casinowar/tree/main/BlackJack"
-                  target="_blank"
-                >
-                  Source Code
-                </a>
-              </span>
-            </p>
-          </div>
-          <div class="project-article">
-            <h3 class="project-tile">Tributes Page</h3>
-            <img
-              src="https://res.cloudinary.com/dsigqr3ht/image/upload/v1693084509/Porfolio/tribute_ymrzlb.png"
-              alt="Tribuntes Picture"
-            />
-            <h4>Languages</h4>
-            <p class="language">
-              <span>HTML</span>
-              <span>CSS</span>
-            </p>
-            <p>
-              <span>
-                <a
-                  href="https://codepen.io/mgphone/details/zYvJPva"
-                  target="_blank"
-                >
-                  Link For Web
-                </a>
-              </span>
-              <span>
-                <a
-                  href="https://codepen.io/mgphone/pen/zYvJPva"
-                  target="_blank"
-                >
-                  Source Code
-                </a>
-              </span>
-            </p>
-          </div> */}
+          {data.map((item) => (
+            <div className={style["project-article"]} key={item.id}>
+              <h3 className={style["project-tile"]}>{item.title}</h3>
+              <img src={item.img} alt={item.title} />
+              <h4>Languages</h4>
+              <p className={style.language}>
+                {item.language.map((item, index) => (
+                  <span key={index}>{item}</span>
+                ))}
+              </p>
+              <p>
+                <span>
+                  <a href={item.weblink} target="_blank">
+                    Link For Web
+                  </a>
+                </span>
+                <span>
+                  <a href={item.sourcecode} target="_blank">
+                    Source Code
+                  </a>
+                </span>
+              </p>
+            </div>
+          ))}
         </div>
       </section>
-      <section id="contact">
+      <section id="contact" className={style["contact"]}>
         <div className={style["contact-header"]}>
           <h2>Contact</h2>
           <p>Let's Work Together on Something Awesome!</p>
@@ -247,7 +99,7 @@ function index() {
           >
             {/* <i className={style["fa-brands fa-linkedin"]}></i> Linkedin */}
             <i className={`${style["fa-brands"]} ${style["fa-linkedin"]}}`}></i>
-            Linkedin
+            LinkedIn
           </a>
           <a
             id="profile-link"
@@ -267,7 +119,10 @@ function index() {
             <i className={`${style.fas} ${style["fa-envelope"]}`}></i> Send a
             mail
           </a>
-          <a href="#" className={`${style.btn} ${style["contact-details"]}`}>
+          <a
+            href="tel:07738518821"
+            className={`${style.btn} ${style["contact-details"]}`}
+          >
             <i className={`${style.fas} ${style["fa-mobile-alt"]}`}></i> Call me
           </a>
         </div>
