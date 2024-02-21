e5262f68c50d8e91ffc608298c431dc62a8b2042
Mgphone <51868037+Mgphone@users.noreply.github.com>
2024-02-21 01:20:25 +0000

    add to github resume

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,7 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-
-  // Add basePath
   basePath: "/resume",
+  output: "export",
+  reactStrictMode: true,
 };
 
 module.exports = nextConfig;
