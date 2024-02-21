435699a3725b0a0e0c5538186ff4c8724e8c3673
Mgphone <51868037+Mgphone@users.noreply.github.com>
2024-02-21 01:39:01 +0000

    change export default nextconfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,48 @@
-module.exports = {
-  async rewrites() {
-    return [{ source: "/:any*", destination: "/" }];
-  },
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  reactStrictMode: true,
 };
+
+export default nextConfig;
+// next.config.js;
+
+// const nextConfig = {
+//   // Other configurations...
+//   exportPathMap: async function (
+//     defaultPathMap,
+//     { dev, dir, outDir, distDir, buildId }
+//   ) {
+//     return {
+//       "/@": { page: "/" },
+//     };
+//   },
+
+//   reactStrictMode: true,
+// };
+
+// module.exports = nextConfig;
+
+// next.config.js
+
+/**
+//  * @type {import('next').NextConfig}
+ */
+// const nextConfig = {
+//   // Specify the export target
+//   target: "experimental-serverless-trace",
+
+//   // Specify the output directory
+//   outDir: "public",
+
+//   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+//   // trailingSlash: true,
+
+//   // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+//   // skipTrailingSlashRedirect: true,
+
+//   // Optional: Change the output directory `out` -> `dist`
+//   // distDir: 'dist',
+// };
+
+// module.exports = nextConfig;
