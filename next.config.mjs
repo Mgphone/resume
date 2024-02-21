6e1ff7247f96ddbd5b5c2f30a6b28d1ba7149c3f
Mgphone <51868037+Mgphone@users.noreply.github.com>
2024-02-21 00:42:05 +0000

    static file output

--- next.config.mjs
+++ next.config.mjs
@@ -7,18 +7,36 @@
 // export default nextConfig;
 // next.config.js
 
-const nextConfig = {
-  // Other configurations...
-  exportPathMap: async function (
-    defaultPathMap,
-    { dev, dir, outDir, distDir, buildId }
-  ) {
-    return {
-      "/@": { page: "/" },
-    };
-  },
+// const nextConfig = {
+//   // Other configurations...
+//   exportPathMap: async function (
+//     defaultPathMap,
+//     { dev, dir, outDir, distDir, buildId }
+//   ) {
+//     return {
+//       "/@": { page: "/" },
+//     };
+//   },
 
-  reactStrictMode: true,
+//   reactStrictMode: true,
+// };
+
+// module.exports = nextConfig;
+
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  output: "export",
+
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
 };
 
 module.exports = nextConfig;
