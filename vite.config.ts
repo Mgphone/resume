1e91ebbb7cdd7703345476ae5c380572bf107031
mgphone <pmn.blazer@gmail.com>
2025-08-22 09:48:45 +0100

    update my viteconfig

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  base: process.env.NODE_ENV === 'production' && process.env.VERCEL_URL ? '/' : '/resume/',
   server: {
     port: 3000,
     open: true,
